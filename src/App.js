cd823c7accf3fa9399f30d7b47d14fa4fdcfdc48
Rudra1125 <77561100+Rudra1125@users.noreply.github.com>
2022-01-18 21:59:18 +0530

    Some Extra changes

--- src/App.js
+++ src/App.js
@@ -19,26 +19,37 @@ function App() {
   const [mode, setMode] = useState('light'); //whether dark mode is enable or no t
   const [alert, setAlert] = useState(null);
 
-  const showAlert =(message,type) =>{
+  const showAlert=(message,type)=>{
     setAlert({
       msg:message,
       type:type
+
     })
     setTimeout(()=>{
       setAlert(null);
-    },1000);
+    },1500);
   }
-  const toggleMode = () =>{
+  const removeBodyClasses =()=>{
+    document.body.classList.remove('bg-light')
+    document.body.classList.remove('bg-dark')
+    document.body.classList.remove('bg-warning')
+    document.body.classList.remove('bg-danger')
+    document.body.classList.remove('bg-success')
+    
+  }
+
+  const toggleMode = (cls)=>{
+    removeBodyClasses();
+    console.log(cls);
+    document.body.classList.add('bg-'+cls);
     if(mode==='light'){
       setMode('dark');
       document.body.style.backgroundColor='#042743';
-      // document.title='TextUtils - Dark Mode';
-      showAlert("Dark mode has been enabled","success")
+
     }else{
       setMode('light');
       document.body.style.backgroundColor='white';
-      showAlert("Light mode has been enabled","success");
-      // document.title='TextUtils - Light Mode';
+      showAlert("light mode has been enabled");
     }
   }
   return (
